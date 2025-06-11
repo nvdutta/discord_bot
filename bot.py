e42cf24655ec5656d76f3bc9b25c6f8f2da7fde1
Nick Dutta <nicholas.v.dutta@gmail.com>
2025-06-11 09:22:24 -0400

    Made bot more evil
    Increased rudeness. Added a catch to remove double spacing from outputs.  Added an LLM call for the sign off message.

--- bot.py
+++ bot.py
@@ -27,6 +27,7 @@ background = """
                 You have been to India and Italy and bring it up often.
                 Your messages are short and to the point. You do not use periods
                 or capitalization.
+                You are sometimes regarded as rude or condescending.
                 You never use the words 'duh', 'obviously', or 'clearly'. 
                 When anime is involved, you feel you are morally superior for not watching it."""
 
@@ -59,7 +60,13 @@ async def on_message(message):
     if message.mentions and client.user in message.mentions and not message.mention_everyone:
 
         if responses_today == max_responses_per_day:
-            await message.reply("zzz")
+            answer = llm.models.generate_content(
+                model=model,
+                contents=f"""
+                {background}
+                Offer a short, vague excuse for why you have to go."""
+            ).text.replace("\n\n", "\n")
+            await message.reply(answer)
             responses_today += 1
             return
     
@@ -75,14 +82,12 @@ async def on_message(message):
                 {chat_history}
                 
                 How do you respond?"""
-            )
+            ).text.replace("\n\n", "\n")
 
-        await message.channel.send(answer.text)
-        ##await message.reply(answer.text)
-        print(f"Sent response: {answer.text}")
-        chat_history += f"**You said: \n{answer.text}\n"
+        await message.reply(answer)
+        print(f"Sent response: {answer}")
+        chat_history += f"**You said: \n{answer}\n"
         responses_today += 1
-        ##print(f"***Full Chat History***: \n{chat_history}")
         return
 
 
@@ -98,13 +103,12 @@ async def on_message(message):
             Answer the following question, in 20 words or fewer.
             ****QUESTION****
             {message.content[5:]}"""
-        )
+        ).text.replace("\n\n", "\n")
         
-        await message.reply(answer.text)
-        print(f"Sent response: {answer.text}")
-        chat_history += f"**You said:\n{answer.text}\n"
+        await message.channel.send(answer)
+        print(f"Sent response: {answer}")
+        chat_history += f"**You said:\n{answer}\n"
         answered_question_today = True
-        ##print(f"***Full Chat History***: \n{chat_history}")
         return
 
 client.run(bot_token)
